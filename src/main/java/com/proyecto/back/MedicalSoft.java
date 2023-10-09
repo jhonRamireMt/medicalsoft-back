56b4955d15361502e536a2dc8fe7920196e67db8
jhon ramirez <neopik182@gmail.com>
2023-10-09 17:24:32 -0500

    New tag

--- src/main/java/com/proyecto/back/MedicalSoft.java
+++ src/main/java/com/proyecto/back/MedicalSoft.java
@@ -2,6 +2,8 @@ package com.proyecto.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
 
 @SpringBootApplication
 public class MedicalSoft {
